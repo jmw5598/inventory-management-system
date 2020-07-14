19a9076170d8bcaf436b39238e190820852f2e43
Jason White <jmw5598@gmail.com>
2020-07-14 10:25:15 -0400

    Added message for failed login exception

--- server/src/authentication/strategies/local.strategy.ts
+++ server/src/authentication/strategies/local.strategy.ts
@@ -12,7 +12,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   public async validate(username: string, password: string): Promise<any> {
     const user = await this.authenticationService.validateUser(username, password);
     if (!user) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException("Invalid username/password");
     }
     return user;
   }
