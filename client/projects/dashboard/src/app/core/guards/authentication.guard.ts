ae4d707cfc24756d3df653ca45dbdc26d0c79de6
Jason White <jmw5598@gmail.com>
2020-07-14 13:27:45 -0400

    Updated authentication guard to use ngrx store

--- client/projects/dashboard/src/app/core/guards/authentication.guard.ts
+++ client/projects/dashboard/src/app/core/guards/authentication.guard.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { take, map } from 'rxjs/operators';
 
 import { AuthenticationService } from '../services/authentication.service';
+import { AuthenticatedStatus } from '../enums/authenticated-status.enum';
+import { IAppState } from '../store/state/app.state';
+import { IAuthenticationState } from '../store/state/authentication.state';
+import { selectAuthenticationState } from '../store/selectors/authentication.selector';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationGuard implements CanActivate {
   constructor(
+    private _store: Store<IAppState>,
     private _authenticationService: AuthenticationService,
     private _router: Router
   ) { } 
@@ -18,10 +25,15 @@ export class AuthenticationGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
-    if (this._authenticationService.isAuthenticated()) {
-      return true;
-    }
-    this._router.navigate(['/auth', 'login']);
-    return false;
+    return this._store.select(selectAuthenticationState)
+      .pipe(
+        take(1), 
+        map((state: IAuthenticationState) => {
+          if (state.authenticatedStatus === AuthenticatedStatus.AUTHENTICATED) {
+            return true;
+          }
+          this._router.navigate(['/auth', 'login']);
+          return false;
+        }));
   }
 }
