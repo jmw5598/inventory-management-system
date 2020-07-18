532708f79266206264406400f793ce765f95e305
Jason White <jmw5598@gmail.com>
2020-07-18 07:36:48 -0400

    Refactored jwt token interceptor and authentication service.
    - Removed all old state properties and methods from authentication service since they wont be need anymore with the ngrx state.
    - Updated jwt token interceptor to use ngrx state for current authenticaiton status and to get the access token to add to Authorization header.

--- client/projects/dashboard/src/app/core/interceptors/jwt-token.interceptor.ts
+++ client/projects/dashboard/src/app/core/interceptors/jwt-token.interceptor.ts
@@ -1,19 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
 
-import { AuthenticationService } from '../services/authentication.service';
+import { IAppState } from '../store/state/app.state';
+import { IAuthenticationState } from '../store/state/authentication.state';
 import { AuthenticatedUser } from '../models/authenticated-user.model';
+import { AuthenticatedStatus } from '../enums/authenticated-status.enum';
+import { selectAuthenticationState } from '../store/selectors/authentication.selector'; 
 
 @Injectable()
-export class JwtTokenInterceptor implements HttpInterceptor {
-  constructor(private _authenticationService: AuthenticationService) { }
+export class JwtTokenInterceptor implements HttpInterceptor, OnDestroy {
+  private _authenticatedState: IAuthenticationState;
+  private _authenticatedStateSubscription: Subscription;
+
+  constructor(private _store: Store<IAppState>) {
+    this._authenticatedStateSubscription = this._store.select(selectAuthenticationState)
+      .subscribe(state => this._authenticatedState = state);
+  }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    const authenticatedUser: AuthenticatedUser = this._authenticationService.getAuthenticatedUser();
-    if (authenticatedUser) {
-      // @@@ TODO - The Bearer prefix will eventually be provided with the access token an refresh token.
-      const authorizationHeaderValue: string = `Bearer ${authenticatedUser.accessToken}`;
+    if (this._authenticatedState.authenticatedStatus === AuthenticatedStatus.AUTHENTICATED) {
+      const prefix: string = this._authenticatedState.authenticatedUser.prefix;
+      const accessToken: string = this._authenticatedState.authenticatedUser.accessToken;
+      const authorizationHeaderValue: string = `${prefix} ${accessToken}`;
       request = request.clone({
         setHeaders: {
           Authorization: authorizationHeaderValue 
@@ -21,5 +31,10 @@ export class JwtTokenInterceptor implements HttpInterceptor {
       });
     }
     return next.handle(request);
+  }
+
+  ngOnDestroy() {
+    if (this._authenticatedStateSubscription)
+      this._authenticatedStateSubscription.unsubscribe();
   }
 }
