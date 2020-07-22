97ed2917dc66036e4c56223ac395325d5b3de3d3
Jason White <jmw5598@gmail.com>
2020-07-22 17:22:11 -0400

    Update manage-stockrooms page to display list of all stockroms with basic summary (value, item count, listed count, unlisted, and actions)
    BUG: Still need to figure out why change detection error is thrown.  Changes were made after view was init?

--- client/projects/dashboard/src/app/dashboard/stockrooms/pages/manage-stockrooms/manage-stockrooms.component.ts
+++ client/projects/dashboard/src/app/dashboard/stockrooms/pages/manage-stockrooms/manage-stockrooms.component.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+
+import { IAppState } from '../../../../core/store/state/app.state';
+import { selectStockroomSummaries } from '../../../../core/store/selectors/stockroom.selector';
+import { StockroomSummary } from '../../../../core/models/stockroom-summary.model';
+import { deleteStockroom } from 'projects/dashboard/src/app/core/store/actions/stockroom.actions';
+
+class StockroomSummaryTotals {
+  public stockroomCount: number = 0;
+  public itemCount: number = 0;
+  public listedCount: number = 0;
+  public purchaseValue: number = 0;
+};
 
 @Component({
   selector: 'inv-manage-stockrooms',
@@ -6,10 +21,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage-stockrooms.component.scss']
 })
 export class ManageStockroomsComponent implements OnInit {
+  public stockroomSummaries$: Observable<StockroomSummary[]>;
+  public stockroomSummaryTotals: StockroomSummaryTotals;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private _store: Store<IAppState>) {
+    this.stockroomSummaryTotals = new StockroomSummaryTotals();
   }
 
+  ngOnInit(): void {
+    this.stockroomSummaries$ = this._store.select(selectStockroomSummaries)
+      .pipe(tap((summaries: StockroomSummary[]) => this._setStockroomSummaryTotals(summaries)));;
+      
+  }
+
+  public onDeleteStockroom(stockroomId: number): void {
+    this._store.dispatch(deleteStockroom({ id: stockroomId }));
+  }
+
+  private _setStockroomSummaryTotals(summaries: StockroomSummary[]): void {
+    console.log("getting stockroom totals for", summaries);
+    this.stockroomSummaryTotals = summaries.reduce((total, summary) => {
+      total.stockroomCount += 1;
+      total.itemCount += summary.itemCount || 0;
+      total.listedCount += summary.listedCount || 0;
+      total.purchaseValue += summary.purchaseValue || 0;
+      return total;
+    }, new StockroomSummaryTotals());
+  }
 }
