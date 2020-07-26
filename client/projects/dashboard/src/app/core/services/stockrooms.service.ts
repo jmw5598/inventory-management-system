8b1c3dafc8badfb7e6d75ebc05febc1558a8058e
Jason White <jmw5598@gmail.com>
2020-07-26 18:41:31 -0400

    Added method to stockrooms service to get stockroom summaries

--- client/projects/dashboard/src/app/core/services/stockrooms.service.ts
+++ client/projects/dashboard/src/app/core/services/stockrooms.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { AbstractCrudService } from './abstract-crud.service';
 import { Stockroom } from '../models/stockroom.model';
 import { environment } from '../../../environments/environment';
+import { StockroomSummary } from '../models/stockroom-summary.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +12,9 @@ import { environment } from '../../../environments/environment';
 export class StockroomsService extends AbstractCrudService<Stockroom, number> {
   constructor(protected _http: HttpClient) {
     super(_http, `${environment.api.base}/stockrooms`); 
+  }
+
+  public getStockroomSummaries(): Observable<StockroomSummary[]> {
+    return this._http.get<StockroomSummary[]>(`${this._base}/summary`);
   }
 }
