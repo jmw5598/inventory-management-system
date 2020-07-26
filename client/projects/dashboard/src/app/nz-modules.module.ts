5dfee4e035c10595634e2ccb8103545db9793f10
Jason White <jmw5598@gmail.com>
2020-07-26 09:19:19 -0400

    updated nz-modules module export additional ng-zorro components

--- client/projects/dashboard/src/app/nz-modules.module.ts
+++ client/projects/dashboard/src/app/nz-modules.module.ts
@@ -1,9 +1,25 @@
 import { NgModule } from '@angular/core';
 
+import { NzAlertModule } from 'ng-zorro-antd/alert';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
 import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzNotificationModule } from 'ng-zorro-antd/notification';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzStatisticModule } from 'ng-zorro-antd/statistic';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NzTypographyModule } from 'ng-zorro-antd/typography'
 
 import { IconDefinition } from '@ant-design/icons-angular';
 import * as AllIcons from '@ant-design/icons-angular/icons';
@@ -20,10 +36,26 @@ const icons: IconDefinition[] = Object.keys(antDesignIcons)
     NzIconModule.forRoot(icons)
   ],
   exports: [
+    NzAlertModule,
+    NzAvatarModule,
     NzBreadCrumbModule,
+    NzButtonModule,
+    NzCardModule,
+    NzCheckboxModule,
+    NzDropDownModule,
+    NzFormModule,
     NzIconModule,
+    NzInputModule,
     NzLayoutModule,
     NzMenuModule,
+    NzNotificationModule,
+    NzPageHeaderModule,
+    NzPopconfirmModule,
+    NzSpinModule,
+    NzStatisticModule,
+    NzTableModule,
+    NzTagModule,
+    NzTypographyModule,
   ]
 })
 export class NgZorroModule {}
