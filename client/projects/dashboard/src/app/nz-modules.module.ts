f2b13b355391dddf2783c46c69a4820a43ab0e8d
Jason White <jmw5598@gmail.com>
2020-07-31 16:10:34 -0400

    Updated ng zorro module to export select module

--- client/projects/dashboard/src/app/nz-modules.module.ts
+++ client/projects/dashboard/src/app/nz-modules.module.ts
@@ -15,6 +15,7 @@ import { NzNotificationModule } from 'ng-zorro-antd/notification';
 import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
 import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
 import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzSelectModule } from 'ng-zorro-antd/select';
 import { NzSpinModule } from 'ng-zorro-antd/spin';
 import { NzStatisticModule } from 'ng-zorro-antd/statistic';
 import { NzTableModule } from 'ng-zorro-antd/table';
@@ -51,6 +52,7 @@ const icons: IconDefinition[] = Object.keys(antDesignIcons)
     NzNotificationModule,
     NzPageHeaderModule,
     NzPopconfirmModule,
+    NzSelectModule,
     NzSpinModule,
     NzStatisticModule,
     NzTableModule,
