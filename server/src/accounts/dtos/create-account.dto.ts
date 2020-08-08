29f31a5a373fdf0b5287dac3f7b3e123981979c6
Jason White <jmw5598@gmail.com>
2020-08-08 07:18:59 -0400

    Updated create account dto after entity updates

--- server/src/accounts/dtos/create-account.dto.ts
+++ server/src/accounts/dtos/create-account.dto.ts
@@ -2,15 +2,6 @@ import { IsNotEmpty, IsEmail } from 'class-validator';
 import { Plan } from '../../plans/entities/plan.entity';
 
 export class CreateAccountDto {
-  @IsEmail()
-  public email: string;
-
-  @IsNotEmpty()
-  public username: string;
-
-  @IsNotEmpty()
-  public password: string;
-
   @IsNotEmpty()
   public plan: Plan;
 }
