12b80fd68a2eb724b5627fe20650677657afedd0
Jason White <jmw5598@gmail.com>
2020-08-14 08:56:26 -0400

    Update ng zorro modules

--- client/projects/dashboard/src/app/nz-modules.module.ts
+++ client/projects/dashboard/src/app/nz-modules.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { NzAlertModule } from 'ng-zorro-antd/alert';
+import { NzAutocompleteModule } from 'ng-zorro-antd/auto-complete'
 import { NzAvatarModule } from 'ng-zorro-antd/avatar';
 import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 import { NzButtonModule } from 'ng-zorro-antd/button';
@@ -15,9 +16,11 @@ import { NzNotificationModule } from 'ng-zorro-antd/notification';
 import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
 import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
 import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
 import { NzSelectModule } from 'ng-zorro-antd/select';
 import { NzSpinModule } from 'ng-zorro-antd/spin';
 import { NzStatisticModule } from 'ng-zorro-antd/statistic';
+import { NzStepsModule } from 'ng-zorro-antd/steps';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import { NzTagModule } from 'ng-zorro-antd/tag';
 import { NzTypographyModule } from 'ng-zorro-antd/typography'
@@ -38,6 +41,7 @@ const icons: IconDefinition[] = Object.keys(antDesignIcons)
   ],
   exports: [
     NzAlertModule,
+    NzAutocompleteModule,
     NzAvatarModule,
     NzBreadCrumbModule,
     NzButtonModule,
@@ -52,9 +56,11 @@ const icons: IconDefinition[] = Object.keys(antDesignIcons)
     NzNotificationModule,
     NzPageHeaderModule,
     NzPopconfirmModule,
+    NzRadioModule,
     NzSelectModule,
     NzSpinModule,
     NzStatisticModule,
+    NzStepsModule,
     NzTableModule,
     NzTagModule,
     NzTypographyModule,
