694aac4635a29009b22d0a620cbf3f1a4ae6ce2d
Jason White <jmw5598@gmail.com>
2020-08-14 09:31:30 -0400

    WIP: Continued building out creating stock items

--- client/projects/dashboard/src/app/dashboard/stock-items/components/stock-item-form/stock-item-form.component.ts
+++ client/projects/dashboard/src/app/dashboard/stock-items/components/stock-item-form/stock-item-form.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { ProductItem, StockItem } from '@inv/core';
 
 @Component({
   selector: 'inv-stock-item-form',
@@ -7,15 +9,54 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./stock-item-form.component.scss']
 })
 export class StockItemFormComponent implements OnInit {
+  public stockItem: StockItem;
+  public productItem: ProductItem;
   public form: FormGroup;
 
   constructor(private _formBuilder: FormBuilder) {
     this.form = this._formBuilder.group({
-
+      productItem: this._formBuilder.group({
+        id: [],
+        title: ['', [Validators.required]],
+        sku: [''],
+        brand: [''],
+        model: [''],
+        category: ['', [Validators.required]]
+      }),
+      // Stock item shouldn't be another group??
+      // Should model the stock-item.model class as this
+      // is what is sent ot the server.
+      stockItem: this._formBuilder.group({
+        id: [],
+        purchaseDate: ['', [Validators.required]]
+      })
     });
+  }
+
+  @Input()
+  public set productItemValue(productItem: ProductItem) {
+    console.log('new product item')
+    this.form.controls.productItem.patchValue(productItem);
+    this.productItem = productItem;
+  }
+
+  @Input()
+  public set stockItemValue(stockItem: StockItem) {
+    this.form.controls.stockItem.patchValue(stockItem);
+    this.stockItem = stockItem; 
   }
 
   ngOnInit(): void {
   }
+  
+  public deselectProductItem(): void {
+    this.form.controls.productItem.reset();
 
+    // Product item should be two way data bound??
+    this.productItem = null;
+  }
+
+  public onSubmit(value: any): void {
+    console.log(value);
+  }
 }
--- client/projects/dashboard/src/app/dashboard/stock-items/pages/create-stock-item/create-stock-item.component.ts
+++ client/projects/dashboard/src/app/dashboard/stock-items/pages/create-stock-item/create-stock-item.component.ts
@@ -1,19 +1,75 @@
-import { Component, OnInit } from '@angular/core';
-import { StockItem } from '@inv/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil, debounceTime, distinctUntilChanged, tap } from 'rxjs/operators';
+
+import { IAppState } from '../../../../core/store/state/app.state';
+import { IPageable, Page, PageRequest, StockItem, ProductItem } from '@inv/core';
+import { searchProductItems } from '../../../../core/store/actions/product-item.actions';
+import { selectProductItemSearchResult } from '../../../../core/store/selectors/product-item-selector';
 
 @Component({
   selector: 'inv-create-stock-item',
   templateUrl: './create-stock-item.component.html',
   styleUrls: ['./create-stock-item.component.scss']
 })
-export class CreateStockItemComponent implements OnInit {
+export class CreateStockItemComponent implements OnInit, OnDestroy {
+  private readonly SEARCH_PAGE_OPTIONS = PageRequest.from(1, 5, 'title', 'ASC');
+  private _subscriptionSubject: Subject<void>;
+  private _autocompleteSearchTextChangeSubject: Subject<string>;
+  public searchResult: Page<ProductItem>;
+  public searchTerm: string = '';
 
-  constructor() { }
+  nzFilterOption = () => true;
+
+  public productItem: ProductItem | string;
+  
+  constructor(private _store: Store<IAppState>) {
+    this._subscriptionSubject = new Subject<void>();
+    this._autocompleteSearchTextChangeSubject = new Subject<string>();
+  }
 
   ngOnInit(): void {
+    // Update store for autocomplete serach values for stock items?
+    // Create endpoint for this search?? or use existing paged search??
+    // select and subscirbe to this piece of state
+    this._store.select(selectProductItemSearchResult)
+      .pipe(takeUntil(this._subscriptionSubject))
+      .subscribe(page => this.searchResult = page)
+   
+    // sub to search change and debounce
+    this._autocompleteSearchTextChangeSubject
+      .pipe(
+        takeUntil(this._subscriptionSubject),
+        debounceTime(500),
+        distinctUntilChanged(),
+        tap(search => this.onFilterStockItems(search))
+      ).subscribe();
   }
 
   public onCreateStockItem(item: StockItem): void {
     console.log("creating ", item);
+  }
+
+  public onSearchStockItemsKeyup(searchTerm): void {
+    this._autocompleteSearchTextChangeSubject.next(searchTerm);
+  }
+
+  public onFilterStockItems(searchTerm: string): void {
+    this._store.dispatch(searchProductItems({
+      searchTerm: searchTerm,
+      pageable: this.SEARCH_PAGE_OPTIONS 
+    }));
+  }
+
+  public onSearchOptionSelected(productItem: ProductItem): void {
+    console.log('selected: ', productItem);
+    this.productItem = productItem;
+    console.log('this.productItem: ', this.productItem);
+  }
+
+  ngOnDestroy() {
+    this._subscriptionSubject.next();
+    this._subscriptionSubject.complete();
   }
 }
