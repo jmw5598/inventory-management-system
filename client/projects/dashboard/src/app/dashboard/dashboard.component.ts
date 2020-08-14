4bf161b7ba8fdca1908a7b58140c26d92884baa6
Jason White <jmw5598@gmail.com>
2020-08-14 08:55:48 -0400

    Updated dashboard layout adding fade animation, and changed icons

--- client/projects/dashboard/src/app/dashboard/dashboard.component.ts
+++ client/projects/dashboard/src/app/dashboard/dashboard.component.ts
@@ -5,11 +5,13 @@ import { Store } from '@ngrx/store';
 import { Stockroom } from '@inv/core';
 import { IAppState } from '../core/store/state/app.state';
 import { selectCurrentStockrooms } from '../core/store/selectors/stockroom.selector';
+import { fadeAnimation } from '../shared/animations/fade-in-out.animation';
 
 @Component({
   selector: 'inv-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  animations: [fadeAnimation]
 })
 export class DashboardComponent implements OnInit {
   public isCollapsed: boolean = false;
