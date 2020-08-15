66f837db56a26f1bfd8cc48126acd75bc1a7864d
Jason White <jmw5598@gmail.com>
2020-08-15 08:25:11 -0400

    Updated dashabord layout.
    - Moved user avatar to right side of header.
    - Propertly display initials in header.  Currently it is showing the first to characters of the username.  Eventually, the first and last name will be used.

--- client/projects/dashboard/src/app/dashboard/dashboard.component.ts
+++ client/projects/dashboard/src/app/dashboard/dashboard.component.ts
@@ -4,8 +4,10 @@ import { Store } from '@ngrx/store';
 
 import { Stockroom } from '@inv/core';
 import { IAppState } from '../core/store/state/app.state';
+import { selectAuthenticatedUser } from '../core/store/selectors/authentication.selector';
 import { selectCurrentStockrooms } from '../core/store/selectors/stockroom.selector';
 import { fadeAnimation } from '../shared/animations/fade-in-out.animation';
+import { AuthenticatedUser } from 'projects/core/src/public-api';
 
 @Component({
   selector: 'inv-dashboard',
@@ -16,11 +18,17 @@ import { fadeAnimation } from '../shared/animations/fade-in-out.animation';
 export class DashboardComponent implements OnInit {
   public isCollapsed: boolean = false;
   public stockroomState$: Observable<Stockroom[]>;
+  public authenticatedUser$: Observable<AuthenticatedUser>;
 
   constructor(private _store: Store<IAppState>) { }
 
   ngOnInit(): void {
     this.stockroomState$ = this._store.select(selectCurrentStockrooms);
+    this.authenticatedUser$ = this._store.select(selectAuthenticatedUser);
+  }
+
+  public getUserInitials(user: AuthenticatedUser): string {
+    return user ? user.userDetails.username.substring(0, 2).toUpperCase() : 'U'
   }
 
   public onSiderCollapse(isCollapsed: boolean): void {
