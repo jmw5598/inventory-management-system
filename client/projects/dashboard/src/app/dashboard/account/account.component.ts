c36eae14ee6a36ad7bc01ec4d0cdbafc6f61fd09
Jason White <jmw5598@gmail.com>
2020-08-19 12:27:37 -0400

    Updated account component to contain layout for subroutes to account-routing module

---
+++ client/projects/dashboard/src/app/dashboard/account/account.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+import { NzTabPosition } from 'ng-zorro-antd/tabs';
+
+import { Account, Profile } from '@inv/core';
+import { IAppState } from '@dashboard/core/store/state';
+import { selectAccountDetails, selectAccountProfile } from '@dashboard/core/store/selectors';
+import { updateAccountProfile, updateAccountDetails } from '@dashboard/core/store/actions';
+
+
+@Component({
+  selector: 'inv-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.scss']
+})
+export class AccountComponent implements OnInit {
+  public accountDetails$: Observable<Account>;
+  public accountProfile$: Observable<Profile>;
+
+  constructor(private _store: Store<IAppState>) { }
+
+  ngOnInit(): void {
+    this.accountDetails$ = this._store.select(selectAccountDetails);
+    this.accountProfile$ = this._store.select(selectAccountProfile);
+  }
+
+  // TODO REMOVE THIS
+  tabs: Array<{ name: string; content: string }> = [
+    {
+      name: 'Profile',
+      content: 'Profile section'
+    },
+    {
+      name: 'Details',
+      content: 'Details seciton'
+    }
+  ];
+  nzTabPosition: NzTabPosition = 'left';
+  selectedIndex = 0;
+
+  /* tslint:disable-next-line:no-any */
+  log(args: any[]): void {
+    console.log(args);
+  }
+}
