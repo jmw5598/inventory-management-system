9582c7ecb233296b657c27fd2e46b20995a7a638
Jason White <jmw5598@gmail.com>
2020-08-25 18:07:48 -0400

    Updated shared forms.  Remove card wrapper around stock-item-form and product-item-form.  Removed h2 tags as well.

--- client/projects/dashboard/src/app/shared/forms/product-item-form/product-item-form.builder.ts
+++ client/projects/dashboard/src/app/shared/forms/product-item-form/product-item-form.builder.ts
@@ -3,7 +3,7 @@ import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 export const buildProductItemFormGroup = (formBuilder: FormBuilder): FormGroup => {
   return formBuilder.group({
     id: [],
-    title: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+    title: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(100)]],
     description: ['', [Validators.required, Validators.minLength(3)]],
     sku: [''],
     make: [''],
