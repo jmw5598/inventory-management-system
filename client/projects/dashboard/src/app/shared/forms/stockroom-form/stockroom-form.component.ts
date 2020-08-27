25051ca48d3a174e877875fbc104f74435da48d5
Jason White <jmw5598@gmail.com>
2020-08-27 19:02:40 -0400

    Fixed issue with locations not being created after refactor

--- client/projects/dashboard/src/app/shared/forms/stockroom-form/stockroom-form.component.ts
+++ client/projects/dashboard/src/app/shared/forms/stockroom-form/stockroom-form.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormArray, ControlContainer, Validators } from '@angular/forms';
+import { 
+  AbstractControl, 
+  FormBuilder, 
+  FormGroup, 
+  FormArray, 
+  ControlContainer, 
+  Validators } from '@angular/forms';
 import { Location } from '@inv/core';
 
 @Component({
@@ -10,6 +16,7 @@ import { Location } from '@inv/core';
 export class StockroomFormComponent implements OnInit {
   public form: FormGroup;
   public locations: FormArray;
+  private _locations: Location[];
 
   constructor(
     private _parentControl: ControlContainer,
@@ -23,6 +30,8 @@ export class StockroomFormComponent implements OnInit {
   }
 
   public addStockroomLocation(location?: Location): void {
+    const stockroom: FormGroup = this.form.get('stockroom') as FormGroup;
+    const locations: FormArray = stockroom.get('locations') as FormArray;
     this.locations.push(
       this._formBuilder.group({
         id: [(location ? location.id : '')],
@@ -32,50 +41,28 @@ export class StockroomFormComponent implements OnInit {
   }
 
   public removeStockroomLocation(index: number): void {
-    const locations: FormArray = this.form.get('locations') as FormArray;
-    if (this.locations.length > 1) {
+    const stockroom: FormGroup = this.form.get('stockroom') as FormGroup;
+    const locations: FormArray = stockroom.get('locations') as FormArray;
+    if (locations.length > 1) {
       this.locations.removeAt(index);
     }
   }
 
   public onAddStockroomLocationsChange(event?): void {
-    if (this.form.value.hasStockroomLocations) {
-      this.form.addControl('locations', this.locations);
+    const stockroom: FormGroup = this.form.get('stockroom') as FormGroup;
+    if (stockroom.value.hasStockroomLocations) {
+      stockroom.addControl('locations', this.locations);
       if (this.locations.controls.length === 0) {
         this.addStockroomLocation();
       }
     } else {
-      this.form.removeControl('locations');
+      stockroom.removeControl('locations');
     }
   }
 
-  private _validateFormGroup(group: FormGroup): void {
-    for (const i in group.controls) {
-      if (group.controls[i] instanceof FormArray) {
-        const formArray: FormArray = group.controls[i] as FormArray;
-        this._validateFormArray(formArray);
-      } else {
-        group.controls[i].markAsDirty();
-        group.controls[i].updateValueAndValidity();
-      }
-    }
-  }
-
-  private _validateFormArray(array: FormArray): void {
-    for (const i in array.controls) {
-      const formGroup: FormGroup = array.controls[i] as FormGroup;
-      this._validateFormGroup(formGroup);
-    }
-  }
-
-  private _patchStockroomLocations(locations: Location[]): void {
-    if (locations && locations.length > 0) {
-      this.form.get('hasStockroomLocations').patchValue(true);
-      this.form.addControl('locations', this.locations);
-      locations.forEach(e => this.addStockroomLocation(e));      
-    } else {
-      this.form.get('hasStockroomLocations').patchValue(false);
-      this.form.removeControl('locations');
-    }
+  public getLocationControls(): AbstractControl[] {
+    const stockroom: FormGroup = this.form.get('stockroom') as FormGroup;
+    const locations: FormArray = stockroom.get('locations') as FormArray;
+    return locations.controls;
   }
 }
