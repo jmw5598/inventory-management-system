81d2b83d960b93b845aae738327c7d4cbe60d15e
Jason White <jmw5598@gmail.com>
2020-08-28 13:52:57 -0400

    Updated update-stockroom-modal to patch through existing stockroom data to modal form

--- client/projects/dashboard/src/app/dashboard/stockrooms/components/update-stockroom-modal/update-stockroom-modal.component.ts
+++ client/projects/dashboard/src/app/dashboard/stockrooms/components/update-stockroom-modal/update-stockroom-modal.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core'; import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core'; 
+import { FormBuilder, FormArray, FormGroup } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { tap, take, filter } from 'rxjs/operators';
 import { NzModalRef } from 'ng-zorro-antd/modal';
 
-import { ResponseMessage, Stockroom } from '@inv/core';
+import { Location, ResponseMessage, Stockroom } from '@inv/core';
 import { IAppState } from '@dashboard/core/store/state';
 import { buildStockroomFormGroup } from '@dashboard/shared/forms';
-import { updateStockroom, setUpdateStockroomResponseMessage } from '@dashboard/core/store/actions';
-import { selectUpdateStockroomResponseMessage } from '@dashboard/core/store/selectors';
+import { updateStockroom, setUpdateStockroomResponseMessage, setSelectedStockroom } from '@dashboard/core/store/actions';
+import { selectSelectedStockroom, selectUpdateStockroomResponseMessage } from '@dashboard/core/store/selectors';
+import { buildLocationFormGroup } from '@dashboard/shared/forms';
 
 export interface UpdateStockroomModalCloseResponse {
   hasStockroomsBeenUpdated: boolean
@@ -40,11 +42,17 @@ export class UpdateStockroomModalComponent implements OnInit {
       .pipe(
         tap((message: ResponseMessage) => {
           if (message) {
-            this.form.reset();
             setTimeout(() => this._store.dispatch(setUpdateStockroomResponseMessage(null)), 3000);
           }
         })
       )
+
+      this._store.select(selectSelectedStockroom)
+        .pipe(
+          filter((stockroom: Stockroom) => !!stockroom),
+          tap((stockroom: Stockroom) => this._patchStockroomToForm(stockroom)),
+          take(1)
+        ).subscribe();
   }
 
   public onSubmit(formValue: { stockroom: Stockroom }): void {
@@ -57,8 +65,35 @@ export class UpdateStockroomModalComponent implements OnInit {
   }
 
   public onCloseModal(): void {
+    this._store.dispatch(setSelectedStockroom(null));
     this._modal.destroy({
       hasStockroomsBeenUpdated: this._hasStockroomsBeenUpdated
     } as UpdateStockroomModalCloseResponse);
+  }
+
+  private _patchStockroomToForm(stockroom: Stockroom): void {
+    const locations: Location[] = stockroom.locations;
+    const stockroomFormGroup: FormGroup = this.form.get('stockroom') as FormGroup;
+    const locationsFormArray: FormArray = stockroomFormGroup.get('locations') as FormArray;
+    this.form.get('stockroom').patchValue({
+      id: stockroom.id,
+      name: stockroom.name,
+      description: stockroom.description
+    });
+    this._patchLocationsToForm(locations);
+  }
+
+  private _patchLocationsToForm(locations: Location[]): void {
+    const stockroomFormGroup: FormGroup = this.form.get('stockroom') as FormGroup;
+    const locationsFormArray: FormArray = stockroomFormGroup.get('locations') as FormArray;
+    if (locations && locations.length > 0) {
+      locations.forEach((location: Location) => {
+        const locationFormGroup: FormGroup = buildLocationFormGroup(this._formBuilder);
+        locationFormGroup.patchValue(location);
+        locationsFormArray.push(locationFormGroup);
+      })
+      stockroomFormGroup.get('hasStockroomLocations').patchValue(true);
+    }
+    stockroomFormGroup.addControl('locations', locationsFormArray);
   }
 }
